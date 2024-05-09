ac8ab905df3d169676e41bd0f962283d9e09eec6
BenAichaMed <benaichamed66@gmail.com>
2024-05-09 18:57:52 +0100

    first commit

---
+++ app/(dashboard)/collections/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { columns } from "@/components/collections/CollectionColumns";
+import { DataTable } from "@/components/custom ui/DataTable";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@radix-ui/react-separator";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const collections = () => {
+  const [loading, setLoading] = useState(true);
+  const [collections, setCollections] = useState([]);
+  const router = useRouter();
+
+  const getCollections = async () => {
+    try {
+      const res = await fetch("/api/collections", {
+        method: "GET",
+      });
+      const data = await res.json();
+      setCollections(data);
+      setLoading(false);
+    } catch (err) {
+      console.log("[collections_GET]", err);
+    }
+  };
+
+  useEffect(() => {
+    getCollections();
+  }, []);
+  return (
+    <div className="px-10 py-5">
+      <div className="flex items-center justify-between">
+        <p className="text-heading2-bold">Collections</p>
+        <Button
+          className="bg-blue-1 text-white"
+          onClick={() => router.push("/collections/new")}
+        >
+          <Plus className="h-4 w-4 mr-2" />
+          Create Collection
+        </Button>
+      </div>
+      <Separator className="bg-grey-1 my-4" />
+      <DataTable columns={columns} data={collections} searchKey="title" />
+    </div>
+  );
+};
+
+export default collections;
